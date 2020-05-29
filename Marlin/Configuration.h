77d3fb01ef2517c2c435046017766795bb31171d
Michael K Johnson <a1237@danlj.org>
2020-05-29 19:01:47 -0400

    I decided I do not like center origin after all

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1089,8 +1089,8 @@
 #define Y_BED_SIZE 330
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -(5 + X_BED_SIZE/2)
-#define Y_MIN_POS -(23 + Y_BED_SIZE/2)
+#define X_MIN_POS -5
+#define Y_MIN_POS -23
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
@@ -1332,7 +1332,7 @@
 // @section homing
 
 // The center of the bed is at (X=0, Y=0)
-#define BED_CENTER_AT_0_0
+//#define BED_CENTER_AT_0_0
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
